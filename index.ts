088edcead57bc985801d67cf053d68315f8ae43a
shagor1994 <shagor1994@gmail.com>
2023-02-26 21:37:05 +0600

    Added support for parsing JSON-encoded request bodies

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
-
 dotenv.config();
 
 const app: Express = express();
+app.use(express.json());
 const port = process.env.PORT||3000;
 
 app.get('/', (req: Request, res: Response) => {
